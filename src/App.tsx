5e486ec9f635cb0bf457a2f393f5ac1637806e03
NOMAD <43680326+derykdamon@users.noreply.github.com>
2026-08-10 21:50:03 -0500

    Route demo1 to full-screen Mappedin experience

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router'
 import SiteLayout from './components/layout/SiteLayout'
 import DashboardPage from './features/activation/DashboardPage'
 import DemoMap from './features/mappedin/DemoMap'
+import MappedinImmersiveDemoPage from './features/mappedin/MappedinImmersiveDemoPage'
 import SynthesiaDemoPage from './features/synthesia/SynthesiaDemoPage'
 import AboutPage from './pages/AboutPage'
 import ContactPage from './pages/ContactPage'
@@ -31,6 +32,7 @@ function App() {
       />
 
       <Route path="/demo0" element={<SynthesiaDemoPage />} />
+      <Route path="/demo1" element={<MappedinImmersiveDemoPage />} />
     </Routes>
   )
 }
